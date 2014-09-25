570a90c8de0a91f584c905e3f9caa784a2db6adf
Ennima <ennima@hotmail.com>
2014-09-24 22:20:00 -0500

    Config Ok, iniciando Login

--- testSSH/routes/users.js
+++ testSSH/routes/users.js
@@ -14,4 +14,10 @@ router.get('/users', function(req, res) {
   res.render('users', { title: "downList.length", conten: "downList" });
   //res.send(req.session.name);
 });
+
+router.post('/users', function(req, res) {
+ // res.render('users', { title: "downList.length", conten: "downList" });
+  //res.send(req.session.name);
+  console.log(req.body);
+});
 module.exports = router;
